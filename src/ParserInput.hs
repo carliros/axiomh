03e48cd19a1761826d449863cf5c2c32e3047238
Carlos Gómez <carliros.g@gmail.com>
2019-08-30 07:53:15 -0400

    Fixing parsing proposition with pChainr

--- src/ParserInput.hs
+++ src/ParserInput.hs
@@ -18,13 +18,23 @@ parseInput :: FilePath -> String -> Proposition
 parseInput filepath fileContent
   = runParser filepath pProposition fileContent
 
-pProposition :: Parser Proposition
-pProposition =  symbol <$> pSymbolProposition
-            <|> negation <$ pSymbol "~" <*> pProposition
---            <|> implies  <$> pProposition <* pSymbol "->" <*> pProposition
+operators = [[("->", implies)]]
+same_prio ops = foldr (<|>) empty [ op <$ pSymbol c | (c, op) <- ops]
 
-pSymbolProposition :: Parser String
-pSymbolProposition = (:[]) <$> pLetter
+pProposition :: Parser Proposition
+pProposition = foldr pChainr pUnitaryProposition (map same_prio operators)
+
+pUnitaryProposition :: Parser Proposition
+pUnitaryProposition =  pSymbolProposition
+                   <|> pNegationProposition
+                   <|> pParens pProposition
+
+pSymbolProposition :: Parser Proposition
+pSymbolProposition = (\c -> symbol [c]) <$> pLetter <* pSpaces
+
+pNegationProposition :: Parser Proposition
+pNegationProposition
+  = negation <$ pSymbol "~" <*> (pSymbolProposition <|> pParens pProposition)
 
 {-
 pAxioms :: Parser Axioms
